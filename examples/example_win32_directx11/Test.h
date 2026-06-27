ad2de9d031532f5736b1ead5e4d45705a122df1a
Shubh Mishra <133102504+ShubhxD69@users.noreply.github.com>
2026-06-27 17:52:40 +0530

    Delete examples directory

--- examples/example_win32_directx11/Test.h
+++
@@ -1,8 +0,0 @@
-#include <windows.h>
-#include <tlhelp32.h>
-#include <string>
-#include <vector>
-#include <filesystem>
-#include <iostream>
-
-
--- examples/example_win32_directx11/wic_texture_loader.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <cstddef>
-
-struct ID3D11Device;
-struct ID3D11ShaderResourceView;
-
-HRESULT CreateShaderResourceViewFromMemory(ID3D11Device* device, const void* data, size_t size, ID3D11ShaderResourceView** out_srv);
